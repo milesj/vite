d852b6c3a12963ee45178c8bf372399c51616d86
Miles Johnson <mileswjohnson@gmail.com>
2024-01-17 16:49:27 -0800

    Polish.

--- packages/vite/src/node/plugins/resolve.ts
+++ packages/vite/src/node/plugins/resolve.ts
@@ -1087,7 +1087,7 @@ function resolveExportsOrImports(
     return true
   })
 
-  // Support `vite` export condition for local packages
+  // Support `vite` condition for local packages
   if (pkg.inWorkspace) {
     conditions.unshift('vite')
   }
